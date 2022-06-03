e8fb3c8d4f0625f615370d7e8eab6df19dda7723
yingyi <jinzhongjia@qq.com>
2022-06-03 20:57:42 +0800

    增加更新后台账户和密码的功能

--- db/system.go
+++ db/system.go
@@ -11,7 +11,7 @@ func (here *Db) systemInit() {
 	var num int64
 	here.db.Model(&System{}).Count(&num)
 	if num == 0 {
-		hash, err := bcrypt.GenerateFromPassword([]byte(""), bcrypt.DefaultCost)
+		hash, err := bcrypt.GenerateFromPassword([]byte("admin888"), bcrypt.DefaultCost)
 		if err != nil {
 			logrus.Error("passwd encrypt failed!")
 			os.Exit(1)
@@ -21,4 +21,24 @@ func (here *Db) systemInit() {
 			Passwd:  string(hash),
 		})
 	}
+}
+
+// 更新后台账户
+func (here *Db) UpdateAccount(oldAccount string, newAccount string) error {
+	db := here.db.Model(&System{
+		Account: oldAccount,
+	}).Update("account", newAccount)
+	return db.Error
+}
+
+// 更新后台密码
+func (here *Db) UpdatePassword(account string, newPassword string) error {
+	hash, err := bcrypt.GenerateFromPassword([]byte(newPassword), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	db := here.db.Model(&System{
+		Account: account,
+	}).Update("account", string(hash))
+	return db.Error
 }
