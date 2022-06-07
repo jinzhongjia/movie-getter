213e8ffcb846ff976d567a8af4a6e57338d97728
yingyi <jinzhongjia@qq.com>
2022-06-07 10:33:39 +0800

    更新前端依赖库，更改请求方式为异步，优化体验

---
+++ dist/assets/Login.9c35fa09.js
@@ -0,0 +1,1 @@
+import{m as r}from"./index.05930539.js";const e={};function n(c,t){return null}var o=r(e,[["render",n]]);export{o as default};
---
+++ dist/assets/User.6b0440d8.js
@@ -0,0 +1,1 @@
+import{m as r}from"./index.05930539.js";const e={};function c(n,t){return null}var s=r(e,[["render",c]]);export{s as default};
