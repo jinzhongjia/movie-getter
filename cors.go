45a0f0172f3a636275ead355dbdcd6ab8daeb58b
jinzhongjia <jinzhongjia@qq.com>
2022-06-27 18:36:14 +0800

    取出无用注释

--- cors.go
+++ cors.go
@@ -10,16 +10,7 @@ func cors() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		method := c.Request.Method               //请求方法
 		origin := c.Request.Header.Get("Origin") //请求头部
-		// var headerKeys []string                  // 声明请求头keys
-		// for k, _ := range c.Request.Header {
-		// 	headerKeys = append(headerKeys, k)
-		// }
-		// headerStr := strings.Join(headerKeys, ", ")
-		// if headerStr != "" {
-		// 	headerStr = fmt.Sprintf("access-control-allow-origin, access-control-allow-headers, %s", headerStr)
-		// } else {
-		// 	headerStr = "access-control-allow-origin, access-control-allow-headers"
-		// }
+
 		if origin != "" {
 
 			c.Header("Access-Control-Allow-Origin", origin) // 这是允许访问所有域
