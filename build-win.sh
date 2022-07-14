4ab8323e42c514235676046b874f138bea955a59
jinzhongjia <jinzhogjia@taiyouxi.cn>
2022-07-14 16:33:22 +0800

    更新构建脚本

--- build-win.sh
+++
@@ -1,1 +0,0 @@
-go build -ldflags="-s -w" -o movie.exe . && upx movie.exe -9
---
+++ build.sh
@@ -0,0 +1,42 @@
+buildLinux(){
+    CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o ${PWD##*/} . && upx ${PWD##*/} -9
+}
+
+buildWin(){
+    CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags="-s -w" -o ${PWD##*/}.exe . && upx ${PWD##*/}.exe -9
+}
+
+buildMac(){
+    CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags="-s -w" -o ${PWD##*/} . && upx ${PWD##*/} -9
+}
+
+help(){
+    echo 'usage: sh build.sh commond
+    - mac   , Compiling programs for Mac 64-bit computers
+    - win   , Compiling programs for Windows 64-bit computers
+    - linux , Compiling programs for linux 64-bit computers'
+}
+
+
+if [ $# == 0 ] || [ $1 == "help" ] || [ $1 == "h" ]
+then
+    help
+elif [ $1 == "win" ]
+then
+    buildWin
+elif [ $1 == "linux" ]
+then
+    buildLinux
+elif [ $1 == "mac" ]
+then
+    buildMac
+else
+    help
+fi
+
+
+
+
+
+
+
