29b8e24a70502739bb5be34803923882f4bdf0bc
jinzhongjia <jinzhongjia@qq.com>
2022-08-28 23:34:56 +0800

    修复导致登陆失败的接口断言错误

--- session/drive/gormDb/session.go
+++ session/drive/gormDb/session.go
@@ -15,8 +15,8 @@ func (s *Session) Init(w http.ResponseWriter, r *http.Request, long bool, kv map
 	if err != nil {
 		logrus.Error(err)
 	}
-	session.Options.SameSite = http.SameSiteNoneMode // 设置cookie的策略
-	session.Options.Secure = true                    // 设置cookie策略
+	//session.Options.SameSite = http.SameSiteNoneMode // 设置cookie的策略
+	//session.Options.Secure = true                    // 设置cookie策略
 	if long {
 		session.Options.MaxAge = 2592000 // 设置存活时间
 	}
@@ -47,8 +47,10 @@ func (s *Session) Get(r *http.Request, key string) interface{} {
 	session, err := s.ss.Get(r, "data")
 	if err != nil {
 		logrus.Error(err)
+
 	}
-	return session.Values[key].(string)
+
+	return session.Values[key]
 }
 
 func (s *Session) Destroy(w http.ResponseWriter, r *http.Request) {
