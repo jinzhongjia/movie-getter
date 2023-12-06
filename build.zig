e779de803036de0e0eeb0d902593a79d1dfc3fc3
jinzhongjia <mail@nvimer.org>
2023-12-06 10:39:46 +0800

    update to build arm64 for linux with musl

--- build.zig
+++ build.zig
@@ -17,6 +17,24 @@ pub fn build(b: *std.Build) !void {
         @panic("sorry, this can just compile for x86_64");
     }
 
+    const linux_arm_cmd = &[_][]const u8{
+        "/bin/sh",
+        "-c",
+        try std.mem.concat(b.allocator, u8, &[_][]const u8{
+            "CGO_ENABLED=1 ",
+            "CC='zig cc -target aarch64-linux-musl' ",
+            "CXX='zig cc -target aarch64-linux-musl' ",
+            "CGO_CFLAGS='-D_LARGEFILE64_SOURCE' ",
+            "GOOS=linux ",
+            "GOARCH=arm64 ",
+            "go ",
+            "build ",
+            "-ldflags='-linkmode=external -extldflags -static -s -w' ",
+            "-o movie-getter_linux-arm64-musl ",
+        }),
+    };
+    const linux_arm_build_cmd = b.addSystemCommand(linux_arm_cmd);
+
     const linux_cmd = &[_][]const u8{
         "/bin/sh",
         "-c",
@@ -30,6 +48,7 @@ pub fn build(b: *std.Build) !void {
             "go ",
             "build ",
             "-ldflags='-linkmode=external -extldflags -static -s -w' ",
+            "-o movie-getter_linux_x86-64_musl ",
         }),
     };
     const linux_build_cmd = b.addSystemCommand(linux_cmd);
@@ -47,8 +66,10 @@ pub fn build(b: *std.Build) !void {
             "go ",
             "build ",
             "-ldflags='-s -w' ",
+            "-o movie-getter_windows_x86-64",
         }),
     };
+
     const windows_build_cmd = b.addSystemCommand(windows_cmd);
 
     const build_step = b.step("build", "build movie");
@@ -56,6 +77,7 @@ pub fn build(b: *std.Build) !void {
     build_step.dependOn(&windows_build_cmd.step);
     build_step.dependOn(&linux_build_cmd.step);
 
+    b.getInstallStep().dependOn(&linux_arm_build_cmd.step);
     b.getInstallStep().dependOn(&windows_build_cmd.step);
     b.getInstallStep().dependOn(&linux_build_cmd.step);
 
